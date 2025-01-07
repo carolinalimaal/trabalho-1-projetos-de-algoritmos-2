3987aa4e232306c828c1b56d00c2fc7e2d1099bc
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2025-01-07 10:52:45 -0300

    Create ArvoreAVL.h

---
+++ questao_1/ArvoreAVL.h
@@ -0,0 +1,41 @@
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+#include <iostream>
+using namespace std;
+
+// Estrutura do nó da árvore
+struct Node {
+  int numero;
+  int altura;
+  Node *esquerda;
+  Node *direita;
+
+  Node(int value)
+      : numero(value), altura(1), esquerda(nullptr), direita(nullptr) {}
+};
+
+// Classe ArvoreAVL que encapsula as operações da árvore
+class ArvoreAVL {
+private:
+  Node *raiz;
+
+  int altura(Node *node);
+  int balanceamento(Node *node);
+  Node *rotacionaDireita(Node *y);
+  Node *rotacionaEsquerda(Node *x);
+  Node *inserir(Node *node, int numero);
+  Node *remover(Node *node, int numero);
+  Node *menorValor(Node *node);
+  bool pesquisar(Node *node, int numero);
+  void ordenar(Node *node);
+
+public:
+  ArvoreAVL();
+  void inserir(int numero);
+  void remover(int numero);
+  bool pesquisar(int numero);
+  void ordenar();
+};
+
+#endif
